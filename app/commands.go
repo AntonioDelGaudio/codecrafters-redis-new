d544a2131014f2aa6a9e523f5988fef1df29ec08
Antonio Del Gaudio <tonydelgaudio@gmail.com>
2025-08-26 15:36:10 +0200

    go back to my shit

--- app/commands.go
+++ app/commands.go
@@ -392,7 +392,7 @@ func lpush(cmds []string, c net.Conn, m bool, bCount int) (bool, []byte) {
 		}
 		writeChan <- wR
 		endSize = <-lc
-
+		fmt.Println("End size now", endSize)
 	}
 	return !m, []byte(parseStringToRESPInt(strconv.Itoa(endSize)))
 }
