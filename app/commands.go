3dfabbd2cfbabc67c755d68e4faa8971a31f62cb
Antonio Del Gaudio <tonydelgaudio@gmail.com>
2025-08-27 17:09:42 +0200

    add zrem command

--- app/commands.go
+++ app/commands.go
@@ -587,6 +587,7 @@ func zrange(cmds []string, c net.Conn, m bool, bCount int) (bool, []byte) {
 			end = elem.rank + end + 1
 		}
 		if start <= end && start < elem.rank {
+			fmt.Println("Evaluating for range: ", elem)
 			for elem.rank > end {
 				fmt.Println("Skipping rank ", elem.rank)
 				elem = elem.smaller
